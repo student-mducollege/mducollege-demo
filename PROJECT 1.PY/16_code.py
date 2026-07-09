2568ec896d3b0a47d1dab95713c8a83166c269b7
Payal Bhardwaj <payalbhardwaj9729@gmial.com>
2026-07-09 19:23:58 +0530

    First commit

--- PROJECT 1.PY/16_code.py
+++
@@ -1,8 +0,0 @@
-with open("PROJECT 1.PY/poem.txt")as f:
- content=f.read()
-if("twinkle" in content):
-    print("the word twinkle is pesent in the content")
-else:
-    print("the word twinkle is not present in the content")
-
-    f.close()
--- PROJECT 1.PY/17_code.py
+++
@@ -1,9 +0,0 @@
-class Programmer :
-   company="Microsoft"
-   def __init__(self,name,salary ,pin): 
-      self.name = name 
-      self.salary = salary
-      self.pin = pin 
-p =Programmer ( " harry",1200000,254100)
-print(p.name,p.salary,p.pin,p.company)
-        
